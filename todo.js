8a41a2ad4b2ef153bc03d2db49a0f569ac6f85ea
gurkan-lite <117160581+gurkan-lite@users.noreply.github.com>
2023-04-06 14:17:16 +0200

    upd

--- todo.js
+++ todo.js
@@ -1,8 +1,7 @@
 //Select elements
 const form = document.getElementById("todoform");
 const todoInput = document.getElementById("boxT");
-
-boxT;
+const todosListEl = document.getElementById("list-todo");
 
 //array
 let todos = [];
@@ -12,6 +11,7 @@ form.addEventListener("submit", function (event) {
   //This prevents our form from refreshing our page
   event.preventDefault();
   saveTodo();
+  renderTodos();
 });
 
 //Save todo
@@ -31,4 +31,42 @@ function saveTodo() {
     todoInput.value = "";
     console.log(todos);
   }
+}
+
+//Render Todo - basicslly makes the items you write show on the screen
+function renderTodos() {
+  // clear elements before re-render
+  todosListEl.innerHTML = "";
+
+  // Render todos
+  todos.forEach((todo, index) => {
+    todosListEl.innerHTML += `
+    <div class="todo" id=${index}> 
+    <p class="">${todo.value}</p>
+      <p class="trash">Remove</p>
+      </div>
+    `;
+  });
+}
+//lägg in senare så man liksom strycker över en grej om man gjort det, 26min i vid
+
+// Click event listener for all todos
+todosListEl.addEventListener("click", (event) => {
+  const target = event.target;
+  const parentElement = target.parentNode;
+
+  const todo = parentElement;
+  const todoId = todo.id;
+
+  console.log(todoId);
+});
+
+// check a todo, first created as an if statement but then made cleaner
+function checkTodo(todoId) {
+  todos = todos.map((todo, index) => ({
+    ...todo, // will copy all the objects dvs value, just written shorter
+    checked: index === todoId ? !todo.checked : todo.checked, // this is made not to repet myself to much
+  }));
+
+  renderTodos();
 }
